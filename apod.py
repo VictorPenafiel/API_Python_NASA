fc5fdad381cc467255ff1785a4ef42105ca67986
VictorPenafiel <victorpenafiel@gmail.com>
2023-04-27 10:52:59 -0600

    problema modulo apod

--- apod.py
+++ apod.py
@@ -3,7 +3,7 @@ from data import API_KEY
 from get_module import get_nasa
 
 def pull_fotos(n, total = 50, api =API_KEY):
-  url = f'https://api.nasa.gov/planetary/apod?count={total}&api key={api}'
+  url = f'https://api.nasa.gov/planetary/apod?count={total}&api_key={api}'
   resultado = get_nasa(url)
   pool_fotos = [elemento for elemento in resultado if elemento['media_type']=='image']
   
